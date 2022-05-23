f7d3557d44efb424cccdc9068dec061c45cff4cf
CryBow <mskolznev@mail.ru>
2022-05-23 19:53:00 +0300

    Добавление библеотек: map(для статистики), chrono( для измерения времени сортировки). Создание функции "Analysts" для определения статистики. Создание функции "PrintAnalysts" для вывода статистики. Были переименованы некоторые переменные. Произведена полная подпись всех функций и основных действий. Добавлено закрытие файлов.

--- EducationPract/EducationPract.cpp
+++ EducationPract/EducationPract.cpp
@@ -3,8 +3,12 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <chrono>
+#include <map>
 
 using namespace std;
+
+//Разбиение текста на слова
 void FindWords(string S, vector<string>& Words) {
     istringstream stream(S);
     string word;
@@ -13,7 +17,7 @@ void FindWords(string S, vector<string>& Words) {
         check = true;
         for (int i = 0; i < word.length(); i++) {
             check = true;
-            if (((word[i] >= 'А' && word[i] <= 'Я') || (word[i] >= 'а' && word[i] <= 'я') || (word[i] >= '0' && word[i] <= '9'))) {
+            if (((word[i] >= -64 && word[i] <= -33) || (word[i] >= -32 && word[i] <= -1) || (word[i] >= '0' && word[i] <= '9'))) {
                 check = false;
             }
             if (check) {
@@ -26,6 +30,7 @@ void FindWords(string S, vector<string>& Words) {
     }
 }
 
+//Сортировка расчёской
 void Raschoska(vector<string>& Words) {
     int step = Words.size() - 1;
     for (step; step >= 1; step /= 1.2473309) {
@@ -36,6 +41,7 @@ void Raschoska(vector<string>& Words) {
     }
 }
 
+//Вывод отсортированных слов
 void PrintWords(vector<string>& Words, ofstream& fresult) {
     for (int i = 0; i < Words.size(); i++) {
         if (i != 0) {
@@ -52,7 +58,23 @@ void PrintWords(vector<string>& Words, ofstream& fresult) {
     }
 }
 
-void input(string& S, ifstream& fin) {
+//Для статистики (количество слов на букву алфавита)
+void Analysts(vector<string>& Words, map<char, int>& statistics) {
+    for (int i = 0; i < Words.size(); i++) {
+        statistics[Words[i][0]] = statistics[Words[i][0]] + 1;
+    }
+}
+
+//Вывод статистикии
+void PrintAnalysts(map<char, int>& statistics, ofstream& fanalysts) {
+    map <char, int> ::iterator it = statistics.begin();
+    for (int i = 0; it != statistics.end(); it++, i++) {
+        fanalysts << it->first << " - " << it->second << endl;
+    }
+}
+
+//Входной текст
+void Input(string& S, ifstream& fin) {
     bool Transfer = false;
     string line;
     while (getline(fin, line)) {
@@ -63,21 +85,47 @@ void input(string& S, ifstream& fin) {
     }
 }
 
+//Основная функция для работы программы
 void Run() {
     string S;
-    ifstream fin("input.txt");
-    ofstream fresult("result.txt");
-    ofstream fanalysts("analysts.txt");
-
-    input(S, fin);
+    /*string way;
+    cout << "Enter name of input file: ";
+    getline(cin, way);
+    ifstream fin(way);*/ //Открытие файла для чтения текста
+    ifstream fin("input.txt"); //Открытие файла для чтения текста
+    ofstream fresult("result.txt"); //Открытие файла для записи отсортированных слов
+    ofstream fanalysts("analysts.txt"); //Открытие файла для записи статистики
+    Input(S, fin); //Вызов функции Input для считывания входного текста
+    fin.close();//Закрытие файла 
     vector <string> Words;
-    FindWords(S, Words);
-    Raschoska(Words);
-    PrintWords(Words, fresult);
+    map<char, int> statistics;
+    FindWords(S, Words); //Вызов функции FindWords для разбиения исходного текста на слова
 
+    auto begin = chrono::steady_clock::now(); // Время начала отсчёта времени сортировки
+    Raschoska(Words); //Вызов функции Raschoska для сортировки расчёской вектора слов. По возрастанию 
+    auto end = chrono::steady_clock::now(); // Конечное время сортировки
+    auto time = chrono::duration_cast<chrono::microseconds>(end - begin).count();//Время сортировки
+
+    if (fresult.is_open()) {
+        fresult << "Слова, отсортированные по алфавиту и по возрастанию:" << endl;
+        PrintWords(Words, fresult); // Вызов функции PrintWords для вывода отсортированных слов
+    }
+    fresult.close();// Закрытие файла для записи отсортированных слов
+
+    if (fanalysts.is_open()) {
+        fanalysts << "Исходный текст: " << endl;
+        fanalysts << S << endl << endl;
+        fanalysts << "Вариант 20: Кирилица, по алфавиту, по возрастанию, учитывать числа, сортировка расчёской." << endl;
+        fanalysts << "Количество слов: " << Words.size() << endl;
+        fanalysts << "Время сортировки: " << time << " микросек." << endl;
+        fanalysts << "Статистика (количество слов на букву алфавита):" << endl;
+        Analysts(Words, statistics); //Вызов функции Analysts для статистики (количество слов на букву алфавита)
+        PrintAnalysts(statistics, fanalysts); //Вызов функции PrintAnalysts для вывода в файл статистики
+    }
+    fanalysts.close();// Закрытие файла для записи статистики 
 }
 
 int main() {
     setlocale(LC_ALL, "ru");
-    Run();
+    Run(); // Вызов функции Run, для начала работы программы
 }
